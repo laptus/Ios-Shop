15fb931c23df583e67a2db47c535ef76a821814e
Alexander <sasha.laptev@gmail.com>
2018-07-02 23:13:41 +0300

    init

---
+++ Shop/ShopTests/ShopTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShopTests.swift
+//  ShopTests
+//
+//  Created by Laptev Sasha on 02/07/2018.
+//  Copyright © 2018 Laptev Sasha. All rights reserved.
+//
+
+import XCTest
+@testable import Shop
+
+class ShopTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
